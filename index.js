b1f1008b6b890e21433165f69bfbc8c0a548e79f
EA - Henrique Silva De Almeida <almeida_henrique01@educadventista.org>
2023-11-27 13:39:49 -0300

    handlebars

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) =>{
-    resposta.send("oi")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
